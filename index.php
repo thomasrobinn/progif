54509cf854eb3f3e19c6af0b5864d9af1ef58a86
thomasrobinn <44975237+thomasrobinn@users.noreply.github.com>
2018-11-15 22:53:41 +0700

    Update index.php
    penambahan komentar dan penjelasan

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-	session_start();
+	session_start(); //start
 
-	if (!isset($_SESSION['access_token'])) {
-		header('Location: login.php');
+	if (!isset($_SESSION['access_token'])) { //jika tidak memiliki access token
+		header('Location: login.php'); //kembali ke login.php
 		exit();
 	}
 ?>
@@ -13,14 +13,15 @@
 	<meta name="viewport"
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Login With Google</title>
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<title>NIM FINDER</title>
 
 </head>
 <body>
 <div class="container" style="margin-top: 100px">
 	<div class="row">
 		<div class="col-md-3">
-			<img style="width: 80%;" src="<?php echo $_SESSION['picture'] ?>">
+			<img style="width: 90%;" src="<?php echo $_SESSION['picture'] ?>"><br><br>
 		</div>
 
 		<div class="col-md-9">
@@ -47,8 +48,19 @@
 						<td><?php echo $_SESSION['gender'] ?></td>
 					</tr>
 				</tbody>
-			</table>
+			</table><br>
+				<form action="retrieve.php" method="post">
+					<a>Find Somebody Information based on NIM !</a><br>
+					<div class="row" >
+							<input type="form" width="100" length="100" placeholder=" Insert NIM Here !" class="form-inline" name="NIM">
+							<input type="submit" value="Search NIM" style="margin-left: 10px">
+					</div>
+				</form>
 		</div>
+		<div class="row" style="margin-left: 10px">
+			<a href="logout.php" class="btn btn-danger">Log-out</a>
+		</div>
+		
 	</div>
 </div>
 </body>
