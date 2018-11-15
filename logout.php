a0f94d85e7e46d25f2f9e7946f217592c7955c30
thomasrobinn <44975237+thomasrobinn@users.noreply.github.com>
2018-11-15 22:58:16 +0700

    Update logout.php
    penambahan komentar dan penjelasan

--- logout.php
+++ logout.php
@@ -1,8 +1,8 @@
 <?php
-	require_once "config.php";
-	unset($_SESSION['access_token']);
-	$gClient->revokeToken();
+	require_once "config.php";//membutuhkan oauth google yang berisi client id , client secret id dan uri yang berada pada google
+	unset($_SESSION['access_token']); //token dinon aktifkan
+	$gClient->revokeToken(); //menghancurkan token
 	session_destroy();
-	header('Location: login.php');
+	header('Location: login.php'); //menuju login.php
 	exit();
 ?>
