79561c5e59ac41fa1d68cc0c295a1fdde9f2b173
thomasrobinn <44975237+thomasrobinn@users.noreply.github.com>
2018-11-20 14:16:22 +0700

    Update config.php
    pada baris kode ke 8 yaitu redirect URI , apabila akan dijalan maka URInya adalah http://localhost/Progif/g-callback.php. Tapi jika dideploy diinternet maka redirect URInya menjadi https://desolate-crag-67928.herokuapp.com/login.php

--- config.php
+++ config.php
@@ -7,4 +7,5 @@
 	$gClient->setApplicationName("NIM FINDER"); //set nama untuk redireksi saat login menggunakan google
 	$gClient->setRedirectUri("http://localhost/Progif/g-callback.php"); //set lokasi file yang dituju setelah login berhasil
 	$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"); //mengambil informasi dari google berupada data pribadi user
+
 ?>
